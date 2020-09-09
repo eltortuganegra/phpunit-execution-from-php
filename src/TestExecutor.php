56b7d1b36cd89acdf8248f8fe1f9213edb791eb1
Jorge Sánchez <jorge.sanchez@digimobil.es>
2020-09-09 07:48:25 +0200

    Refactor TestExecutor to return a void value

--- src/TestExecutor.php
+++ src/TestExecutor.php
@@ -24,7 +24,7 @@ class TestExecutor
      * @param string $sourceCode
      * @throws TestFileNotFound
      */
-    public function execute(string $testFilePath, string $sourceCode): bool
+    public function execute(string $testFilePath, string $sourceCode): void
     {
         if ( ! is_file($testFilePath)) {
             throw new TestFileNotFound();
@@ -35,13 +35,12 @@ class TestExecutor
         }
 
         // Create a random name for the class
-        $testFileBasename = pathinfo($testFilePath, PATHINFO_BASENAME);
+        $testFileBasename = pathinfo($testFilePath, PATHINFO_FILENAME);
         $sourceFileName = str_replace('Test', 'SourceCode', $testFileBasename);
 
+        // Create kata source code file
         $testFilePath = new TestFile($this->temporaryfilesPath);
         $testFilePath->save($sourceFileName, $sourceCode);
-
-        return true;
     }
 
 }
