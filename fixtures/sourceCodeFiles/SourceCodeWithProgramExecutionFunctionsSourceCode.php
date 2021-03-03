34cddd3b78461051d768ba12357862fe636684c2
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2021-03-03 08:41:28 +0100

    Avoid php code injection #124

--- fixtures/sourceCodeFiles/SourceCodeWithProgramExecutionFunctionsSourceCode.php
+++ fixtures/sourceCodeFiles/SourceCodeWithProgramExecutionFunctionsSourceCode.php
@@ -7,18 +7,18 @@ class SourceCodeWithProgramExecutionFunctionsSourceCode
 {
     public function functionToTest()
     {
-        $programaExecutionFunctions = [
-            'escapeshellarg',
-            'escapeshellcmd',
-            'exec',
-            'passthru',
-            'proc_close',
-            'proc_get_status',
-            'proc_nice',
-            'proc_open',
-            'proc_terminate',
-            'shell_exec',
-            'system'
+        $programExecutionFunctions = [
+            'escapeshellarg(',
+            'escapeshellcmd(',
+            'exec(',
+            'passthru(',
+            'proc_close(',
+            'proc_get_status(',
+            'proc_nice(',
+            'proc_open(',
+            'proc_terminate(',
+            'shell_exec(',
+            'system(',
         ];
 
         return true;
--- src/CodeInjectionValidator.php
+++ src/CodeInjectionValidator.php
@@ -10,6 +10,7 @@ class CodeInjectionValidator
     protected $programExecutionFunctions;
     private $sourceCode;
     private string $executionOperator;
+    private array $fileSystemFunctions;
 
     public function __construct()
     {
@@ -27,6 +28,89 @@ class CodeInjectionValidator
             'system'
         ];
         $this->executionOperator = '`';
+        $this->fileSystemFunctions = [
+            'basename',
+            'chgrp',
+            'chmod',
+            'chown',
+            'clearstatcache',
+            'copy',
+            'delete',
+            'dirname',
+            'disk_free_space',
+            'disk_total_space',
+            'diskfreespace',
+            'fclose',
+            'feof',
+            'fflush',
+            'fgetc',
+            'fgetcsv',
+            'fgets',
+            'fgetss',
+            'file_exists',
+            'file_get_contents',
+            'file_put_contents',
+            'file',
+            'fileatime',
+            'filectime',
+            'filegroup',
+            'fileinode',
+            'filemtime',
+            'fileowner',
+            'fileperms',
+            'filesize',
+            'filetype',
+            'flock',
+            'fnmatch',
+            'fopen',
+            'fpassthru',
+            'fputcsv',
+            'fputs',
+            'fread',
+            'fscanf',
+            'fseek',
+            'fstat',
+            'ftell',
+            'ftruncate',
+            'fwrite',
+            'glob',
+            'is_dir',
+            'is_executable',
+            'is_file',
+            'is_link',
+            'is_readable',
+            'is_uploaded_file',
+            'is_writable',
+            'is_writeable',
+            'lchgrp',
+            'lchown',
+            'link',
+            'linkinfo',
+            'lstat',
+            'mkdir',
+            'move_uploaded_file',
+            'parse_ini_file',
+            'parse_ini_string',
+            'pathinfo',
+            'pclose',
+            'popen',
+            'readfile',
+            'readlink',
+            'realpath_cache_get',
+            'realpath_cache_size',
+            'realpath',
+            'rename',
+            'rewind',
+            'rmdir',
+            'set_file_buffer',
+            'stat',
+            'symlink',
+            'tempnam',
+            'tmpfile',
+            'touch',
+            'umask',
+            'unlink',
+        ];
     }
 
     /**
@@ -38,6 +122,7 @@ class CodeInjectionValidator
         $this->setSourceCode($sourceCode);
         $this->checkIfProgramExecutionFunctionsAreFound();
         $this->checkIfAnExecutionOperatorIsFound();
+        $this->checkIfFileSystemFunctionsAreFound();
     }
 
     private function setSourceCode(string $sourceCode)
@@ -47,24 +132,24 @@ class CodeInjectionValidator
 
     /**
      * @param string $sourceCode
-     * @param string $programExecutionFunction
+     * @param string $programExecutionFunctionCall
      * @throws PossibleCodeInjectionDetected
      */
-    public function checkIfTheProgramExecutionFunctionIsFound(string $programExecutionFunction): void
+    public function checkIfTheProgramExecutionFunctionIsFound(string $programExecutionFunctionCall): void
     {
-        if ($this->isProgramExecutionFunctionFound($programExecutionFunction)) {
+        if ($this->isProgramExecutionFunctionFound($programExecutionFunctionCall)) {
             throw new PossibleCodeInjectionDetected();
         }
     }
 
     /**
      * @param string $sourceCode
-     * @param string $programExecutionFunction
+     * @param string $programExecutionFunctionCall
      * @return bool
      */
-    public function isProgramExecutionFunctionFound(string $programExecutionFunction): bool
+    public function isProgramExecutionFunctionFound(string $programExecutionFunctionCall): bool
     {
-        return strpos($this->sourceCode, $programExecutionFunction) !== false;
+        return strpos($this->sourceCode, $programExecutionFunctionCall) !== false;
     }
 
     /**
@@ -73,7 +158,10 @@ class CodeInjectionValidator
     public function checkIfProgramExecutionFunctionsAreFound(): void
     {
         foreach ($this->programExecutionFunctions as $programExecutionFunction) {
-            $this->checkIfTheProgramExecutionFunctionIsFound($programExecutionFunction);
+            $sourceCodeFunctionCall = $programExecutionFunction . '(';
+            $this->checkIfTheProgramExecutionFunctionIsFound($sourceCodeFunctionCall);
+            $sourceCodeFunctionCall = $programExecutionFunction . ' (';
+            $this->checkIfTheProgramExecutionFunctionIsFound($sourceCodeFunctionCall);
         }
     }
 
@@ -83,6 +171,31 @@ class CodeInjectionValidator
     public function checkIfAnExecutionOperatorIsFound(): void
     {
         if (strpos($this->sourceCode, $this->executionOperator) !== false) {
+            throw new PossibleCodeInjectionDetected();
+        }
+    }
+
+    /**
+     * @param string $sourceCode
+     * @throws PossibleCodeInjectionDetected
+     */
+    public function checkIfFileSystemFunctionsAreFound(): void
+    {
+        foreach ($this->fileSystemFunctions as $fileSystemFunction) {
+            $fileSystemFunctionCall = $fileSystemFunction . '(';
+            $this->checkIfFileSystemFunctionCallIsFound($fileSystemFunctionCall);
+            $fileSystemFunctionCall = $fileSystemFunction . ' (';
+            $this->checkIfFileSystemFunctionCallIsFound($fileSystemFunctionCall);
+        }
+    }
+
+    /**
+     * @param string $fileSystemFunctionCall
+     * @throws PossibleCodeInjectionDetected
+     */
+    public function checkIfFileSystemFunctionCallIsFound(string $fileSystemFunctionCall): void
+    {
+        if (strpos($this->sourceCode, $fileSystemFunctionCall !== false)) {
             throw new PossibleCodeInjectionDetected();
         }
     }
